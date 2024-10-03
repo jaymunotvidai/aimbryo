0cf7e3177a7b2dd4e1abbcdc64aeb7ea273c04df
jayesh <jayesh.munot@vidaisolutions.com>
2024-10-03 16:49:10 +0530

    ui responsive changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,7 +18,7 @@ import Group from "../assets/images/Group 92262 (1).svg";
 import Useradd from "../assets/images/user-add.svg";
 import Clock from "../assets/images/clock.svg";
 function Home() {
-  const { Title, Text } = Typography;
+  const { Title } = Typography;
 
  
   const count = [
@@ -57,20 +57,20 @@ function Home() {
   return (
     <>
       <div className="layout-content">
-        <Row className="rowgap-vbox" gutter={[24, 0]}>
+        <Row className="rowgap-vbox" gutter={[12, 0]}>
           {count.map((c, index) => (
             <Col
               key={index}
               xs={24}
               sm={24}
-              md={12}
-              lg={6}
+              md={24}
+              lg={12}
               xl={6}
               className="mb-24"
             >
               <Card bordered={false} className="criclebox ">
                 <div className="number">
-                  <Row align="middle" gutter={[24, 0]}>
+                  <Row align="middle" gutter={[0, 0]}>
                     <Col xs={5}>
                       <div className="icon-box"><img src={c.icon} alt=''/></div>
                     </Col>
@@ -79,9 +79,7 @@ function Home() {
                         {c.title} <small className={c.bnb}></small>
                       </Title>
                       <span>{c.today}</span>
-
                     </Col>
-
                   </Row>
                 </div>
               </Card>
@@ -91,7 +89,7 @@ function Home() {
 
         <Row gutter={[24, 0]}>
           {/* Left column for Echart and LineChart */}
-          <Col xs={24} sm={24} md={24} lg={7} xl={7} className="mb-24" style={{paddingRight:'0px'}}>
+          <Col xs={24} sm={24} md={24} lg={24} xl={7} className="mb-24" style={{paddingRight:'0px'}}>
             <Card bordered={false} className="criclebox">
             <Overview/>
 
@@ -104,7 +102,7 @@ function Home() {
           </Col>
 
           {/* Right column for the two cards */}
-          <Col xs={24} sm={24} md={24} lg={17} xl={17} className="mb-24" >
+          <Col xs={24} sm={24} md={24} lg={24} xl={17} className="mb-24" >
             {/* First card - shown twice */}
             <div className="d-flex w-100">
               <Card bordered={false} className="criclebox cardbody h-full" style={{ width: '50%',marginRight: '12px', paddingLeft:'12px', height:'500px' }}>
